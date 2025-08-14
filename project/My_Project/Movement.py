064e432d92cb88d8c2edf6b9865e84d055f1814a
OwenLeigh <owen.leigh@education.nsw.gov.au>
2025-08-14 10:57:21 +1000

    started comlicatedmovement and progress on other compoents
    started the base code for complicate movements and upgraded my ultrasonic sensor code and simple movement code

--- project/My_Project/Movement.py
+++ project/My_Project/Movement.py
@@ -1,6 +1,7 @@
 from servo import Servo
 from machine import PWM
 import time
+import PiicoDev_Ultrasonic
 
 class Simple_Movement:
     def __init__ (self, right_Servo, left_Servo):
@@ -27,16 +28,42 @@ class Simple_Movement:
         self.__right_Servo.set_duty(1200)
         self.__left_Servo.set_duty(1200)
 
-class Ultrasonic:
+class Ultrasonic:               #ultrasonic_sensor
     def __init__ (self, range_a, range_b):
-        self.__range_a = range_1 #check foward distance
-        self.__range_b = range_2 #check side distance
+        self.__range_a = range_a
+        self.__range_b = range_b 
 
-    def Dis_Stop(self):
+    def Distance(self):
         self.__range_a.distance_mm (id=[0, 0, 0, 0])
         self.__range_b.distance_mm (id=[1, 0, 0, 0])
 
-    def Dis_Move(self):
+    def Forward_dis(self):
+        return int(self.__range_a.distance_mm) 
 
-class Complicated_Movement:
-    def __init__(self, ):
+    def Side_dis(self):
+        return int(self.__range_b.distance_mm)
+
+class Combination:             
+    def __init__(self, foward, side, state):
+        self.__foward = foward
+        self.__side = side
+        self.__state = "sumtingwong"
+
+    def set_idle(self):
+        Simple_Movement.Idle
+
+    def foward(self):
+        Simple_Movement.Move_forward
+
+
+    def turn_right(self):
+        Simple_Movement.R_Turn
+
+
+    def turn_left(self):
+        Simple_Movement.L_Turn
+
+    def back(self):
+        Simple_Movement.Backward
+
+
